143ce3a2b23fbaf62a1962f62301bd1542de9250
samirapashayeva <106215977+samirapashayeva@users.noreply.github.com>
2022-09-03 09:57:20 -0400

    My day schedule is redy to be anomsed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+      MyDay monday =new MyDay();
+      monday.morning="early";
+      monday.day="sunny";
+      monday.time=9;
+
+        System.out.println("My Monday schedule is ready ");
 
 
 
